12dd1d3c9c9c4f7b14e986ca604f9d252269d67a
ivanov111 <48509769+ivanov111@users.noreply.github.com>
2020-03-12 19:17:40 +0300

    Add files via upload

---
+++ MyCalc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MyCalcLib;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace MyCalcLib
+{
+    public class MyCalc
+    {
+        public bool Sum(string x)
+        {
+            /*string[] mass = {"Чайковского", "Ленина", "Советская", "Гоголя"};*/
+
+            using (FileStream fstream = File.OpenRead(@"F:\c# test\Ulica\ylic1.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string tt = System.Text.Encoding.Default.GetString(array);
+                string q = "'" + x + "'";
+                if (tt.Contains(q))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ MyCalcTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLib.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum123()
+        {
+            string x = "ул.Свободы";
+
+            MyCalc c = new MyCalc();
+            bool ac = c.Sum(x);
+
+            Assert.AreEqual(true, ac);
+        }
+    }
+}
